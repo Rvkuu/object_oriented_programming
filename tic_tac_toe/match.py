3509f4f5bcc50eaa6494bdb6356dd4a7a901b83c
Rvkuu <preciousubi.sam@gmail.com>
2025-07-28 16:09:03 +0200

    tic tac toe

--- tic_tac_toe/match.py
+++ tic_tac_toe/match.py
@@ -11,7 +11,7 @@ class Match:
         self.current_index = 1 - self.current_index
 
     def play(self):
-        print("🎮 Welcome to Tic Tac Toe!")
+        print("Welcome to Tic Tac Toe!")
         self.table.display()
 
         while True:
